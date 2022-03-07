48497d13795d8c4b589bc144b620ed98607b7778
Class <a13777666@gmail.com>
2022-03-07 17:16:52 +0800

    third commit

--- app/src/main/java/com/yencheng/bmi2/MainActivity.kt
+++ app/src/main/java/com/yencheng/bmi2/MainActivity.kt
@@ -2,7 +2,9 @@ package com.yencheng.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.yencheng.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //They are same
+        /*binding.bHelp.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })*/
+        binding.bHelp.setOnClickListener {
+            Log.d("MainActivity","Help Click")
+        }
     }
 
     fun bmi(view: View){
         var weight = binding.edWeight.text.toString().toFloat()
         var height = binding.edHeight.text.toString().toFloat()
-        println(weight/(height*height))
+        var bmi = weight/(height*height)
+        Log.d("MainActivity", bmi.toString())
+        //Toast.makeText(this,"Your BMI", Toast.LENGTH_LONG).show()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Hello")
+        builder.setMessage("Your BMI is $bmi")
+        builder.setPositiveButton("OK",null)
+        val dialog = builder.create()
+        dialog.show()
+
+
+        /*AlertDialog.Builder(this)
+            .setTitle("Hello")
+            .setMessage("Your BMI is $bmi")
+            .setPositiveButton("OK", null)
+            .show()
+        */
+        //binding.tvBmi.text = "Your BMI is $bmi"
+
     }
+
 }
